0795ba39156948e12b0e29270a23528e99f016ba
Sebastian Diaz <seadiaz@gmail.com>
2019-01-15 07:21:48 -0300

    chore(dependency): add script to up kafka

---
+++ kafka.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker run -d \
+  --name=zookeeper \
+  -e ZOOKEEPER_CLIENT_PORT=2181 \
+  -p 2181:2181 \
+  confluentinc/cp-zookeeper:5.1.0
+
+docker run -d \
+  --name=kafka \
+  -p 9092:9092 \
+  -e KAFKA_ZOOKEEPER_CONNECT=10.0.1.66:2181 \
+  -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://10.0.1.66:9092 \
+  -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 \
+  confluentinc/cp-kafka:5.1.0
